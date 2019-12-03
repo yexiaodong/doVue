cc4a1c5bef390e968f4b91322567e73c3c1b1f90
yexiaodong <2283138757@qq.com>
2019-12-03 08:53:36 +0800

    全部

---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'doVue',
+    description: 'vue 基础框架说明文档',
+    themeConfig: {
+        sidebar: 'auto',
+        nav: [
+          { text: '首页', link: '/' },
+          { text: '组件', link: '/components/' },
+          { text: '工具', link: '/tools/' },
+          { text: '样式', link: '/sass/' },
+          { text: '其他', link: '/others/' },
+          { text: '关于我们', link: '/about/' },
+        ],
+
+    }
+}
