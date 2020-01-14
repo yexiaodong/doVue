dd98b5c324b605d2c49e8908b0221a27b1a9dcd9
yexiaodong <2283138757@qq.com>
2020-01-14 16:34:29 +0800

    配置

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,16 +1,17 @@
 module.exports = {
-    title: 'doVue',
-    description: 'vue 基础框架说明文档',
-    themeConfig: {
-        sidebar: 'auto',
-        nav: [
-          { text: '首页', link: '/' },
-          { text: '组件', link: '/components/' },
-          { text: '工具', link: '/tools/' },
-          { text: '样式', link: '/sass/' },
-          { text: '其他', link: '/others/' },
-          { text: '关于我们', link: '/about/' },
-        ],
+  base: 'doVue',//github部署路径
+  title: 'doVue',
+  description: 'vue 基础框架说明文档',
+  themeConfig: {
+    sidebar: 'auto',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '组件', link: '/components/' },
+      { text: '工具', link: '/tools/' },
+      { text: '样式', link: '/sass/' },
+      { text: '其他', link: '/others/' },
+      { text: '关于我们', link: '/about/' },
+    ],
 
-    }
+  }
 }
